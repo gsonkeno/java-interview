36dfccdeabf2805b4f8181a5e46d23c794d82538
gaosong <805940564@qq.com>
2019-03-03 18:16:18 +0800

    init

--- src/main/java/com/gsonkeno/interview/HashMap1.java
+++ src/main/java/com/gsonkeno/interview/HashMap1.java
@@ -7,6 +7,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  *
  * jdk7扩容时都可能导致死锁
  * jdk8在PutTreeValue时可能死循环   死循环在hashMap的1816行， java version "1.8.0_111"
+ * jstack发现可能卡在 at java.util.HashMap$TreeNode.balanceInsertion(HashMap.java:2229)
+ * 也有可能卡在 at java.util.HashMap$TreeNode.balanceInsertion(HashMap.java:2229)
  * @author gaosong
  * @since 2019-02-23
  */
@@ -33,10 +35,11 @@ class HashMapThread extends Thread
     @Override
     public void run()
     {
-        while (ai.get() < 10000000)
+        while (ai.get() < 100000)
         {
             map.put(ai.get(), ai.get());
             ai.incrementAndGet();
         }
+        System.out.println(Thread.currentThread().getName() + "执行结束完");
     }
 }
