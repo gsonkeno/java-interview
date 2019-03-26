b30e92cf52f482abccd85659f92293466200af00
高嵩 <gaosong@vvic.com>
2019-03-26 11:19:20 +0800

    java四种引用

--- src/main/java/com/gsonkeno/interview/jvm/understandJvm/chapter3/MinorGC.java
+++ src/main/java/com/gsonkeno/interview/jvm/understandJvm/chapter3/MinorGC.java
@@ -1,6 +1,6 @@
 package com.gsonkeno.interview.jvm.understandJvm.chapter3;
 /**
- *
+ * https://segmentfault.com/a/1190000010648021
  * -verbose:gc -Xms20m -Xmx20m -Xmn10m -XX:+PrintGCDetails -XX:SurvivorRatio=8
  * @author gaosong
  * @since 2019-03-26
@@ -10,11 +10,15 @@ public class MinorGC {
     private static final int _1MB=1024*1024;
 
     public static void main(String[] args) throws InterruptedException {
-        byte[] a1,a2,a3,a4;
-        a1 = new byte[2*_1MB];
-        a2 = new byte[2*_1MB];
-        //a3 = new byte[2*_1MB];
-        //a4 = new byte[4*_1MB]; //出现一次Minor GC
+
+        long a = 1;
+        long b =2;
+        byte[] a1 = new byte[2*_1MB];
+        byte[] a2 = new byte[2*_1MB];
+        byte[] a3 = new byte[2*_1MB];
+        byte[] a4 = new byte[4*_1MB];
+
         //Thread.sleep(Integer.MAX_VALUE);
+
     }
 }
