cd4c9c6e336d14bee261eac4f59b6e10e1719be9
gaosong <805940564@qq.com>
2019-06-20 23:42:39 +0800

    增加jdk-proxy测试

---
+++ proxy-spring-aop/src/main/java/com/gsonkeno/springaop/Performance.java
@@ -0,0 +1,10 @@
+package com.gsonkeno.springaop;
+
+/**
+ * Created by gaosong on 2017-12-01
+ */
+public interface Performance {
+    public void perform(String style);
+
+    public void feel(Light light);
+}
